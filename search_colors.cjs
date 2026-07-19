7bc581bd7acfa6ae735b8d889f5e6fc7004c569f
Sha <pianetasimts4@gmail.com>
2026-07-19 12:49:03 +0200

    Test26

---
+++ search_colors.cjs
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const html = fs.readFileSync('index.html', 'utf8');
+
+const lines = html.split('\n');
+lines.forEach((line, index) => {
+  if (line.includes('catColorsList')) {
+    console.log(`${index + 1}: ${line.trim()}`);
+  }
+});
